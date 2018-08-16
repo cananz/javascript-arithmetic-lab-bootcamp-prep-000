16ca31aa9f2c6edbf6e48e2e5d25166bb6c7b752
Marisa Canan-Zucker <mcananzucker@gmail.com>
2018-08-16 22:55:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+function inc(n) {
+  
+}
+function dec(n) {
+  
+}
+function makeInt(n) {
+  
+}
+function preserveDecimal(n) {
+  
+}
